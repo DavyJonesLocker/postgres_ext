f5e09265ff3cf65ba1a6d42c5c55c7c61d157a76
Dan Seaver <git@danseaver.com>
2012-05-03 13:12:34 -0400

    Adds macaddr migration specs

--- spec/migrations/data_types_spec.rb
+++ spec/migrations/data_types_spec.rb
@@ -44,6 +44,27 @@ describe 'Native Data Types Migrations' do
       cidr_3.sql_type.should eq 'cidr'
     end
   end
+
+  describe 'macaddr type' do
+    it 'creates an macaddr column' do
+      lambda do
+        Testing.connection.create_table :data_types do |t|
+          t.macaddr :macaddr_1
+          t.macaddr :macaddr_2, :macaddr_3
+        end
+      end.should_not raise_exception
+
+      columns = Testing.connection.columns(:data_types)
+      macaddr_1 = columns.detect { |c| c.name == 'macaddr_1'}
+      macaddr_2 = columns.detect { |c| c.name == 'macaddr_2'}
+      macaddr_3 = columns.detect { |c| c.name == 'macaddr_3'}
+
+      macaddr_1.sql_type.should eq 'macaddr'
+      macaddr_2.sql_type.should eq 'macaddr'
+      macaddr_3.sql_type.should eq 'macaddr'
+    end
+  end
+
   after do
     Object.send(:remove_const, :Testing)
   end
