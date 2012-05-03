af9e1febba166598f7a45116b545fd28e384550c
Dan Seaver <git@danseaver.com>
2012-05-03 12:16:13 -0400

    Fixes debugger requires to be only 1.9.3

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -1,5 +1,5 @@
 Dummy::Application.configure do
-  require 'debugger'
+  require 'debugger' if RUBY_VERSION == '1.9.3'
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
