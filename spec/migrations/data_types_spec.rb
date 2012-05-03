e429d3642abdf4c9e19c79b78a1291946f1fb2c7
Dan Seaver <git@danseaver.com>
2012-05-03 11:21:02 -0400

    Adds test for cidr migrations

--- spec/migrations/data_types_spec.rb
+++ spec/migrations/data_types_spec.rb
@@ -22,10 +22,28 @@ describe 'Native Data Types Migrations' do
       ip_1.sql_type.should eq 'inet'
       ip_2.sql_type.should eq 'inet'
       ip_3.sql_type.should eq 'inet'
-      ip_1.sql_type.should eq 'inet'
     end
   end
 
+  describe 'cidr type' do
+    it 'creates an cidr column' do
+      lambda do
+        Testing.connection.create_table :data_types do |t|
+          t.cidr :cidr_1
+          t.cidr :cidr_2, :cidr_3
+        end
+      end.should_not raise_exception
+
+      columns = Testing.connection.columns(:data_types)
+      cidr_1 = columns.detect { |c| c.name == 'cidr_1'}
+      cidr_2 = columns.detect { |c| c.name == 'cidr_2'}
+      cidr_3 = columns.detect { |c| c.name == 'cidr_3'}
+
+      cidr_1.sql_type.should eq 'cidr'
+      cidr_2.sql_type.should eq 'cidr'
+      cidr_3.sql_type.should eq 'cidr'
+    end
+  end
   after do
     Object.send(:remove_const, :Testing)
   end
