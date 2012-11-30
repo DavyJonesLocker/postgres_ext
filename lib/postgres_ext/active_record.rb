2d42ce44873d7e1009c6075f569f6f267fc5e6b1
Dan McClain <git@danmcclain.net>
2012-11-30 10:00:47 -0500

    Fixes #34 - #update_column with an array value
    Monkeypatches ActiveRecord::Sanitization

--- lib/postgres_ext/active_record.rb
+++ lib/postgres_ext/active_record.rb
@@ -1,2 +1,3 @@
+require 'postgres_ext/active_record/sanitization'
 require 'postgres_ext/active_record/connection_adapters'
 require 'postgres_ext/active_record/schema_dumper'
---
+++ lib/postgres_ext/active_record/sanitization.rb
@@ -0,0 +1,30 @@
+require 'active_record/sanitization'
+
+module ActiveRecord
+  module Sanitization
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def sanitize_sql_hash_for_assignment(attrs)
+        attrs.map do |attr, value|
+          "#{connection.quote_column_name(attr)} = #{quote_bound_value(value, attr)}"
+        end.join(', ')
+      end
+
+      def quote_bound_value(value, column = nil, c = connection)
+        if column.present?
+          record_column = self.columns.select {|col| col.name == column}.first
+          c.quote(value, record_column)
+        elsif value.respond_to?(:map) && !value.acts_like?(:string)
+          if value.respond_to?(:empty?) && value.empty?
+            c.quote(nil)
+          else
+            value.map { |v| c.quote(v) }.join(',')
+          end
+        else
+          c.quote(value)
+        end
+      end
+    end
+  end
+end
