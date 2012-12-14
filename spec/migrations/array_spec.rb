08a936e2530bde642a658d40bf8ce5067b3a31b8
Dan McClain <git@danmcclain.net>
2012-12-14 08:51:46 -0500

    Better organization of migration specs for arrays

--- spec/migrations/array_spec.rb
+++ spec/migrations/array_spec.rb
@@ -1,47 +1,66 @@
 require 'spec_helper'
 
-describe 'array migrations' do
+describe 'Array migrations' do
   let!(:connection) { ActiveRecord::Base.connection }
-  it 'creates an cidr column' do
-    lambda do
-      connection.create_table :data_types do |t|
-        t.cidr :cidr_1, :array => true
-        t.cidr :cidr_2, :cidr_3, :array => true
-        t.column :cidr_4, :cidr, :array => true
+
+  context 'New Table' do
+    after { connection.drop_table :data_types }
+    describe 'Create table methods' do
+      it 'creates an array column' do
+        lambda do
+          connection.create_table :data_types do |t|
+            t.cidr :array_1, :array => true
+            t.cidr :array_2, :array_3, :array => true
+            t.column :array_4, :cidr, :array => true
+          end
+        end.should_not raise_exception
+
+        columns = connection.columns(:data_types)
+
+        array_1 = columns.detect { |c| c.name == 'array_1'}
+        array_2 = columns.detect { |c| c.name == 'array_2'}
+        array_3 = columns.detect { |c| c.name == 'array_3'}
+        array_4 = columns.detect { |c| c.name == 'array_4'}
+
+        array_1.sql_type.should eq 'cidr[]'
+        array_2.sql_type.should eq 'cidr[]'
+        array_3.sql_type.should eq 'cidr[]'
+        array_4.sql_type.should eq 'cidr[]'
       end
-    end.should_not raise_exception
+    end
+  end
 
-    lambda do
-      connection.add_column :data_types, :cidr_5, :cidr, :array => true
-    end.should_not raise_exception
-
-    lambda do
-      connection.change_table :data_types do |t|
-        t.column :cidr_6, :cidr, :array => true
+  context 'Existing Table' do
+    before { connection.create_table :data_types }
+    after  { connection.drop_table   :data_types }
+    describe 'Add Column' do
+      it 'creates an array column' do
+        lambda do
+          connection.add_column :data_types, :array_5, :cidr, :array => true
+        end.should_not raise_exception
+        columns = connection.columns(:data_types)
+        array_5 = columns.detect { |c| c.name == 'array_5'}
+        array_5.sql_type.should eq 'cidr[]'
       end
-    end.should_not raise_exception
+    end
 
-    lambda do
-      connection.change_table :data_types do |t|
-        t.cidr :cidr_7, :array => true
+    describe 'Change table methods' do
+      it 'creates an array column' do
+        lambda do
+          connection.change_table :data_types do |t|
+            t.column :array_6, :cidr, :array => true
+            t.cidr :array_7, :array => true
+          end
+        end.should_not raise_exception
+
+        columns = connection.columns(:data_types)
+
+        array_6 = columns.detect { |c| c.name == 'array_6'}
+        array_7 = columns.detect { |c| c.name == 'array_7'}
+
+        array_6.sql_type.should eq 'cidr[]'
+        array_7.sql_type.should eq 'cidr[]'
       end
-    end.should_not raise_exception
-
-    columns = connection.columns(:data_types)
-    cidr_1 = columns.detect { |c| c.name == 'cidr_1'}
-    cidr_2 = columns.detect { |c| c.name == 'cidr_2'}
-    cidr_3 = columns.detect { |c| c.name == 'cidr_3'}
-    cidr_4 = columns.detect { |c| c.name == 'cidr_4'}
-    cidr_5 = columns.detect { |c| c.name == 'cidr_5'}
-    cidr_6 = columns.detect { |c| c.name == 'cidr_6'}
-    cidr_7 = columns.detect { |c| c.name == 'cidr_7'}
-
-    cidr_1.sql_type.should eq 'cidr[]'
-    cidr_2.sql_type.should eq 'cidr[]'
-    cidr_3.sql_type.should eq 'cidr[]'
-    cidr_4.sql_type.should eq 'cidr[]'
-    cidr_5.sql_type.should eq 'cidr[]'
-    cidr_6.sql_type.should eq 'cidr[]'
-    cidr_7.sql_type.should eq 'cidr[]'
+    end
   end
 end
