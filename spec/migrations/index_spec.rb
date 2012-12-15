90fd7aede74f0ac6c0af46c7eb79bd5d24ba46f4
Dan McClain <git@danmcclain.net>
2012-12-15 07:54:12 -0500

    Adds spec for index creation with index_opclass

--- spec/migrations/index_spec.rb
+++ spec/migrations/index_spec.rb
@@ -8,16 +8,21 @@ describe 'index migrations' do
       connection.create_table :index_types do |t|
         t.integer :col1, :array => true
         t.integer :col2
+        t.text    :col3
       end
       connection.add_index(:index_types, :col1, :index_type => :gin)
       connection.add_index(:index_types, :col2, :where => '(col2 > 50)')
+      connection.add_index(:index_types, :col3, :index_type => :gin, :index_opclass => :gin_trgm_ops)
     end.should_not raise_exception
 
     indexes = connection.indexes(:index_types)
     index_1 = indexes.detect { |c| c.columns.map(&:to_s) == ['col1']}
     index_2 = indexes.detect { |c| c.columns.map(&:to_s) == ['col2']}
+    index_3 = indexes.detect { |c| c.columns.map(&:to_s) == ['col3']}
 
-    index_1.index_type.to_s.should eq 'gin'
-    index_2.where.should match /col2 > 50/
+    index_1.index_type.to_s.should  eq    'gin'
+    index_2.where.should            match /col2 > 50/
+    index_3.index_type.should       eq    :gin
+    index_3.index_opclass.should    eq    :gin_trgm_ops
   end
 end
