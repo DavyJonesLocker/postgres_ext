67d68df390566bcec4530b1b632832a6e769b5a1
Dan McClain <git@danmcclain.net>
2012-12-15 08:46:55 -0500

    Better organizes migration specs for indexes

--- spec/migrations/index_spec.rb
+++ spec/migrations/index_spec.rb
@@ -2,26 +2,47 @@ require 'spec_helper'
 
 describe 'index migrations' do
   let!(:connection) { ActiveRecord::Base.connection }
+
+  before do
+    connection.create_table :index_types do |t|
+      t.integer :col1, :array => true
+      t.integer :col2
+      t.text    :col3
+    end
+  end
+
   after { connection.drop_table :index_types }
+
   it 'creates special index' do
     lambda do
-      connection.create_table :index_types do |t|
-        t.integer :col1, :array => true
-        t.integer :col2
-        t.text    :col3
-      end
       connection.add_index(:index_types, :col1, :index_type => :gin)
-      connection.add_index(:index_types, :col2, :where => '(col2 > 50)')
-      connection.add_index(:index_types, :col3, :index_type => :gin, :index_opclass => :gin_trgm_ops)
     end.should_not raise_exception
 
     indexes = connection.indexes(:index_types)
     index_1 = indexes.detect { |c| c.columns.map(&:to_s) == ['col1']}
-    index_2 = indexes.detect { |c| c.columns.map(&:to_s) == ['col2']}
-    index_3 = indexes.detect { |c| c.columns.map(&:to_s) == ['col3']}
 
     index_1.index_type.to_s.should  eq    'gin'
+  end
+
+  it 'creates indexes with where clauses' do
+    lambda do
+      connection.add_index(:index_types, :col2, :where => '(col2 > 50)')
+    end.should_not raise_exception
+
+    indexes = connection.indexes(:index_types)
+    index_2 = indexes.detect { |c| c.columns.map(&:to_s) == ['col2']}
+
     index_2.where.should            match /col2 > 50/
+  end
+
+  it 'creates indexes with operator classes' do
+    lambda do
+      connection.add_index(:index_types, :col3, :index_type => :gin, :index_opclass => :gin_trgm_ops)
+    end.should_not raise_exception
+
+    indexes = connection.indexes(:index_types)
+    index_3 = indexes.detect { |c| c.columns.map(&:to_s) == ['col3']}
+
     index_3.index_type.should       eq    :gin
     index_3.index_opclass.should    eq    :gin_trgm_ops
   end
