ddd570ade84ac281f477d7e57b15d05eacbafc28
Dan McClain <git@danmcclain.net>
2012-12-15 08:43:44 -0500

    Better organizes schema dumper specs for indexes

--- spec/schema_dumper/index_spec.rb
+++ spec/schema_dumper/index_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
-describe 'index schema dumper' do
+describe 'Index schema dumper' do
   let!(:connection) { ActiveRecord::Base.connection }
-  after { connection.drop_table :index_types }
-  it 'correctly generates index statements' do
+
+  before do
     connection.create_table :index_types do |t|
       t.integer :col1, :array => true
       t.integer :col2
       t.text    :col3
     end
+  end
+
+  after { connection.drop_table :index_types }
+
+  it 'handles index type parameters' do
     connection.add_index(:index_types, :col1, :index_type => :gin)
-    connection.add_index(:index_types, :col2, :where => '(col2 > 50)')
-    connection.add_index(:index_types, :col3, :index_type => :gin, :index_opclass => :gin_trgm_ops)
 
     stream = StringIO.new
     ActiveRecord::SchemaDumper.dump(connection, stream)
@@ -19,7 +22,25 @@ describe 'index schema dumper' do
 
     output.should match /:index_type => :gin/
     output.should_not match /:index_type => :btree/
+  end
+
+  it 'handles index where clauses' do
+    connection.add_index(:index_types, :col2, :where => '(col2 > 50)')
+
+    stream = StringIO.new
+    ActiveRecord::SchemaDumper.dump(connection, stream)
+    output = stream.string
+
     output.should match /:where => "\(col2 > 50\)"/
+  end
+
+  it 'dumps index operator classes' do
+    connection.add_index(:index_types, :col3, :index_type => :gin, :index_opclass => :gin_trgm_ops)
+
+    stream = StringIO.new
+    ActiveRecord::SchemaDumper.dump(connection, stream)
+    output = stream.string
+
     output.should match /:index_type => :gin,\s+:index_opclass => :gin_trgm_ops/
   end
 end
