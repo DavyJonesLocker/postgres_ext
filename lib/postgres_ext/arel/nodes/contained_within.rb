fb01a7e1156535266aec8c35b49d91280552e829
Patrick Muldoon <doon@labratsoftware.com>
2013-01-29 08:40:21 -0500

    Adding More Network Functions to Arel
    Adding Arel visitors, and predicates for:
     - contained_within_or_equals (<<=)
     - contains (>>)
     - contains_or_equals (>>=)

--- lib/postgres_ext/arel/nodes/contained_within.rb
+++ lib/postgres_ext/arel/nodes/contained_within.rb
@@ -5,6 +5,18 @@ module Arel
       def operator; :<< end
     end
 
+    class ContainedWithinEquals < Arel::Nodes::Binary
+      def operator; '<<='.symbolize end
+    end
+
+    class Contains < Arel::Nodes::Binary
+      def operator; :>> end
+    end
+
+    class ContainsEquals < Arel::Nodes::Binary
+      def operator; '>>='.symbolize end
+    end
+
     class ArrayOverlap < Arel::Nodes::Binary
       def operator; '&&' end
     end
