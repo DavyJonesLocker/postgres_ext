a34d78cc43940a81dda1d9a95c47e1eb8cb16789
Azamat Khudaygulov <azamat@hudaygulov.ru>
2014-12-06 07:45:45 +0300

    For ActiveRecord 4.2 change class ActiveRecord::PredicateBuilder::ArrayHandler instead of changing the class ActiveRecord::PredicateBuilder.

--- lib/postgres_ext/active_record/relation.rb
+++ lib/postgres_ext/active_record/relation.rb
@@ -1,2 +1,12 @@
+## TODO: Change to ~> 4.2.0 on gem release
+
+gdep = Gem::Dependency.new('activerecord', '~> 4.2.0.beta4')
+ar_version_cutoff = gdep.matching_specs.sort_by(&:version).last
+
 require 'postgres_ext/active_record/relation/query_methods'
-require 'postgres_ext/active_record/relation/predicate_builder'
+if ar_version_cutoff
+  require 'postgres_ext/active_record/relation/predicate_builder/array_handler'
+else
+  require 'postgres_ext/active_record/relation/predicate_builder'
+end
+
